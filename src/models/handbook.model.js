afeb315fbc823332a7a9255841a6b806ae22d50c
PhamVinhPhat <pvphat12c6ntt@gmail.com>
2021-10-10 21:52:27 +0700

    updateAccident-handbook

---
+++ src/models/handbook.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+const Schema = mongoose.Schema
+
+const handbookSchema = new Schema({
+
+  nameHandbook: {
+    type: String,
+  },
+
+  severity: {
+    type: String,
+    enum: ['serious','medium','simple']
+  },
+
+  icon: String,
+
+  content: {
+    type: String,
+    default: "Sẽ cập nhật sau"
+  },
+
+  utensil: {
+    type: String,
+  }
+
+},{
+  timestamps: true,
+});
+/**
+ * @typedef
+ */
+handbookSchema.plugin(toJSON);
+handbookSchema.plugin(paginate);
+
+const Handbook = mongoose.model('Handbook', handbookSchema);
+module.exports = Handbook
+
+
