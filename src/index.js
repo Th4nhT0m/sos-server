b32af959a931665e39e5e0032b25bae9aa098209
Th4nhT0m <tomcoder0129@gmail.com>
2021-11-29 16:07:40 +0700

    add_socket

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 const express = require('express');
 const socketIo = express();
-  const http = require('http').Server(socketIo);
+const http = require('http').Server(socketIo);
 const io = require('socket.io');
 const ConnectSocket = io(http);
 
@@ -17,19 +17,10 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   httpServer.listen(config.port,() => {
     console.log(`Server is running at PORT ${config.port}`);
   });
-  ConnectSocket.on('connection', (ConnectSocket)=>{
-    console.log(ConnectSocket.id);
+  ConnectSocket.on('connection', (socket)=>{
+    console.log(socket.id);
+    socket.emit("hello", "world");
   });
-  ConnectSocket.on('helper', (ConnectSocket)=>{
-    console.log('Helper'+ConnectSocket.id);
-  });
-  // ConnectSocket.on('getAllAccidents',() => {
-  //   ConnectSocket.emit('setAllAccidents',()=>{
-  //     console.log('123');
-  //   })
-  //
-  // });
-
   http.listen(3000, () => {
     console.log('connected to port: ' + 3000)
 
