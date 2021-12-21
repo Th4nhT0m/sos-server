40b8b0039f0ccff80255416683a375d0edfd2adf
Th4nhT0m <tomcoder0129@gmail.com>
2021-12-21 23:27:14 +0700

    sokcet

--- src/index.js
+++ src/index.js
@@ -3,49 +3,85 @@ const app = require('./app');
 const {createServer} = require('http')
 const config = require('./config/config');
 const logger = require('./config/logger');
-const express = require('express');
-const socketIo = express();
-const http = require('http').Server(socketIo);
 const io = require('socket.io');
-const ConnectSocket = io(http);
-
-let server;
-let socket;
+const { log } = require('winston');
+const { User, Socket } = require('./models');
+const { connectSocket, getUserSocketById } = require('./services/socket.server');
+const { socketServer } = require('./services');
+const httpSocket = createServer(app);
+const ConnectSocket = io(httpSocket);
 const httpServer = createServer(app);
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   httpServer.listen(config.port,() => {
     console.log(`Server is running at PORT ${config.port}`);
   });
-  ConnectSocket.on('connection', (socket)=>{
-    console.log(socket.id);
-    socket.emit("hello", "world");
-  });
-  http.listen(3000, () => {
+  httpSocket.listen(3000, () => {
     console.log('connected to port: ' + 3000)
+    ConnectSocket.on('connection',async socket => {
+      console.log('Id socket : ' + socket.id);
+      console.log(socket.handshake.auth.token);
+      const user = await getUserById(socket.handshake.auth.token);
+      socket.request.user = user;
+      await Socket.create ({
+          userID: socket.handshake.auth.token,
+          socketID: socket.id,
+      })
+      socket.on('sentHelp',data =>{
+      if (socket.request.user ===null)
+      {
+        console.log('No connection Socket');
+      }else {
+        socket.broadcast.emit('getHelp',data);
+        console.log('Helper-----------');
+      }
+      });
 
+      socket.on('sentAccidents',data =>{
+        if (socket.request.user === null)
+        {
+          console.log('No connection Socket');
+        }else {
+          socket.emit('getAccidents',data);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log('Disconnected socket connection: '+socket.id);
+        socketServer.deleteSocketById(socket.request.user.id);
+      })
+      //
+      socket.on("connection_error", (err) => {
+        console.log(err.req);      // the request object
+        console.log(err.code);     // the error code, for example 1
+        console.log(err.message);  // the error message, for example "Session ID unknown"
+        console.log(err.context);  // some additional error context
+      });
+
+
+      // socket.on('sentHelp',data =>{
+      //     console.log(data);
+      // console.log(socket.handshake.auth.token);
+      // if (socket.handshake.auth.token ===null)
+      // {
+      //   console.log('No authentication')
+      // }else {
+      //   console.log(data)
+      //   socket.emit('getHelp',data);
+      // }
+      // });
+//
+    });
   });
 });
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info('Server closed');
-      process.exit(1);
-    });
-  } else if(socket){
-    socket.close(() => {
-      logger.info('socket closed');
-      process.exit(1);
-    });
-  }else {
-    process.exit(1);
-  }
+const getUserById = async (id) => {
+  return User.findById(id);
 };
+
+
 
 const unexpectedErrorHandler = (error) => {
   logger.error(error);
-  exitHandler();
 };
 
 process.on('uncaughtException', unexpectedErrorHandler);
@@ -53,7 +89,4 @@ process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
   logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
 });
